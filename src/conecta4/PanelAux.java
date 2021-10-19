38e9b69db1f8e731879636a232c87ac33f8d3d62
antony horteales <ahortealesm1900@alumno.ipn.mx>
2021-10-18 23:41:36 -0500

    connect 4

---
+++ src/conecta4/PanelAux.java
@@ -0,0 +1,15 @@
+package conecta4;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+
+public class PanelAux extends JPanel
+{
+	public PanelAux(int tamano)
+	{
+            setPreferredSize(new Dimension(0,tamano));
+	}
+
+}
